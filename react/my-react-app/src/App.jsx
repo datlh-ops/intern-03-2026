11cfd30c3a38ce6aef6cca73fed1afdae2478783
luudat8401 <luudat8401@gmail.com>
2026-03-09 17:26:32 +0700

    web cho thue

--- react/my-react-app/src/App.jsx
+++ react/my-react-app/src/App.jsx
@@ -1,8 +1,42 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import "./styles/layout.css"
+
+function Dashboard() {
+  return <h1>Trang chủ</h1>;
+}
+function Rooms() {
+  return <h1>Phòng trọ</h1>;
+}
+function Users() {
+  return <h1>Khách thuê trọ</h1>;
+}
+
+function Masters() {
+  return <h1>Chủ trọ</h1>;
+}
+
+function Contracts() {
+  return <h1>Hợp đồng cho thuê</h1>;
+}
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <BrowserRouter>
+
+      <Layout>
+
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/rooms" element={<Rooms />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/masters" element={<Masters />} />
+          <Route path="/contracts" element={<Contracts />} />
+        </Routes>
+
+      </Layout>
+
+    </BrowserRouter>
   );
 }
 
---
+++ react/my-react-app/src/components/Layout.jsx
@@ -0,0 +1,22 @@
+import Sidebar from "./Sidebar";
+import Header from "./Header";
+
+export default function Layout({ children }) {
+  return (
+    <div className="layout">
+
+      <Sidebar />
+
+      <div className="main">
+
+        <Header />
+
+        <div className="content">
+          {children}
+        </div>
+
+      </div>
+
+    </div>
+  );
+}
